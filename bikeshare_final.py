3c47ebabc375902a6505a364f847c52fc0ec8729
John Styers <john.styers28@gmail.com>
2021-01-19 19:59:36 -0600

    Improved documentation (comment string bikeshare_final.py)

--- bikeshare_final.py
+++ bikeshare_final.py
@@ -32,6 +32,7 @@ def get_filters():
     str (city): name of the city to analyze
     str (month): name of the month to filter by, or "all" to apply no month filter
     str (day): name of the day of week to filter by, or "all" to apply no day filter
+    Bat beefs!!!!
     """
     print('Let\'s explore some US bikeshare data.')
 
